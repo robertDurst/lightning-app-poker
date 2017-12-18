fec07fb8f6cd8ef60934ac13e81ebfad87abfd31
natedana <natepdana@gmail.com>
2017-12-18 15:24:14 -0800

    asdf

--- packages/lightning-core/landing/styles.js
+++ packages/lightning-core/landing/styles.js
@@ -1,56 +1,3 @@
-<<<<<<< HEAD
-// const styles = reactCSS({
-//   'default': {
-//     box: {
-//       flex: (props.style && props.style.flex) || props.flex,
-//       height: (props.style && props.style.height) || props.height,
-//       width: (props.style && props.style.width) || props.width,
-//       opacity: (props.style && props.style.opacity) || props.opacity,
-//       borderRadius: (props.style && props.style.borderRadius) || props.borderRadius,
-//       transition: 'box-shadow 100ms ease-out'
-//     }
-//   }}
-// )
-// .container {
-//   background-color: #4CAF50;
-//   height: 100%;
-//   width: 100%;
-//   display: flex;
-//   flex-direction: column;
-// }
-// .container_header {
-//   flex: 1;
-// }
-// .container_body {
-//   flex:10;
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-//   flex-direction: column;
-// }
-// .title_container {
-//   flex: 5;
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-// }
-// .title {
-//   font-size: 100pt;
-//   font-family: "Poker Font";
-//   font-weight: lighter;
-// }
-// .connect_button_container {
-//   flex: 1;
-//   display: flex;
-//   justify-content: center;
-//   align-items: center;
-// }
-// .container--footer {
-//   flex: 1;
-// }
-//
-// export default styles
-=======
 import reactCSS from 'reactcss'
 
 export default reactCSS({
@@ -64,4 +11,3 @@ export default reactCSS({
     }
   },
 })
->>>>>>> 2f328fc35f63514f425708d5ba0556085f608d7e
