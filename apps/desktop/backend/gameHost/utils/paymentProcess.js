3d9c037f1fd693414cfc2eda3163244c16fd3c7e
robertdurst <rsdurst@colby.edu>
2017-12-21 22:30:09 -0800

    Dsirtibution

--- apps/desktop/backend/gameHost/utils/paymentProcess.js
+++ apps/desktop/backend/gameHost/utils/paymentProcess.js
@@ -13,7 +13,7 @@ export function betInvoice(memo, cb) {
   lightning_socket.on('BET_INVOICE', (recievedMemo, invoice) => {
     if (recievedMemo === memo) {
       cb(invoice)
-    } 
+    }
   })
 }
 
@@ -24,3 +24,7 @@ export function completePayment(originalMemo, cb) {
     }
   })
 }
+
+// export function test() {
+//   lightning_socket.emit("GAME_END", "lnsb280u1pdrefxhpp5w605ep52nwf0vkwp03caxm5e7j4rtqnt0zdcw7uk3gthz33ygpasdq8w3jhguccqzysu4utnv6wrmqqgq6ffs6y6g53t2tsl6gzzvvuvzs4056jp6nj4fwsnvwjh00nqfajutmtm37wshw4p0uq2asptcv83kdqturdetc5r5cqfhj00d")
+// }
